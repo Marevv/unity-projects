2b99ff58d236a2c78e711888a6303e306ba12800
Dariusz Marchewka <d.marchewka@vrtechnology.pl>
2023-06-02 14:07:23 +0200

    Finished Tutorial 1.3

--- Assets/Scripts/Rotate.cs
+++ Assets/Scripts/Rotate.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Rotate : MonoBehaviour
 {
     [SerializeField] float angle = 10f;
@@ -10,5 +11,12 @@ public class Rotate : MonoBehaviour
     void Update()
     {
         transform.Rotate(Vector3.up, angle * Time.deltaTime);
+
+        var gameControllers = new List<UnityEngine.XR.InputDevice>();
+
+        foreach(var device in gameControllers)
+        {
+            //if(device.TryGetFeatureValue(UnityEngine.XR.CommonUsages)
+        }
     }
 }
