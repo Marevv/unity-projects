a8f9108a5dc51a319627b3a3ccf310040fd4e94c
Dariusz Marchewka <d.marchewka@vrtechnology.pl>
2023-06-28 15:57:26 +0200

    QuestSystemTutorialResources

---
+++ Assets/Scripts/QuestSystem/QuestInfoSO.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "QuestInfoSO", menuName = "ScriptableObjects/QuestInfoSO", order = 1)]
+public class QuestInfoSO : ScriptableObject
+{
+    [field: SerializeField] public string id { get; private set; }
+
+    [Header("General")]
+    public string displayName;
+
+    [Header("Requirements")]
+    public int levelRequirement;
+
+    public QuestInfoSO[] questPrerequisites;
+
+    [Header("Steps")]
+    public GameObject[] questStepPrefabs;
+
+    [Header("Rewards")]
+    public int goldReward;
+    public int experienceReward;
+
+
+    private void OnValidate()
+    {
+        #if UNITY_EDITOR
+        id = this.name;
+        UnityEditor.EditorUtility.SetDirty(this);
+        #endif
+    }
+}
